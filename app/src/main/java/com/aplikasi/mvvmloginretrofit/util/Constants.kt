081423de336758d148c144fef89ba957a030e99f
Adam Alfarizi Ismail <adam.alfarizi.2002@gmail.com>
2022-11-08 09:50:35 +0700

    Nine Commit

--- app/src/main/java/com/aplikasi/mvvmloginretrofit/util/Constants.kt
+++ app/src/main/java/com/aplikasi/mvvmloginretrofit/util/Constants.kt
@@ -2,7 +2,7 @@ package com.aplikasi.mvvmloginretrofit.util
 
 object Constants {
 
-    const val BASE_URL = "http://192.168.0.102/LaravelProject/api-oauth/public/api/"
+    const val BASE_URL = "http://192.168.40.120/LaravelProject/api-oauth/public/api/"
     const val LOGIN_URL = "login"
     const val REGISTER_URL = "register"
     const val UPDATE_DATA = "update-user/{id}"
