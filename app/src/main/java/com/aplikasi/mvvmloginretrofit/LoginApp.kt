fe1ef7ce1cf32ce52f4cf6f8e4cd6236ef7dde61
asus <adam.alfarizi.2002@gmail.com>
2023-09-09 16:04:32 +0700

    Fixing Apps.kt file

--- app/src/main/java/com/aplikasi/mvvmloginretrofit/LoginApp.kt
+++ app/src/main/java/com/aplikasi/mvvmloginretrofit/LoginApp.kt
@@ -1,11 +1,7 @@
 package com.aplikasi.mvvmloginretrofit
 
-import android.app.Activity
 import android.app.Application
-import android.content.pm.ActivityInfo
-import android.os.Bundle
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class LoginApp: Application() {
-}
+class LoginApp: Application()
